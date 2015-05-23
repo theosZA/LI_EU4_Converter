9b18eb8b6b8d2ab59a3ecaeabaf346b1bb296875
theosZA <theosZA+github@googlemail.com>
2015-05-23 21:50:05 +0200

    Path and output names now parsed from the command line arguments.

--- LI_EU4_Converter.cpp
+++ LI_EU4_Converter.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <stdexcept>
+#include <string>
+#include <vector>
 
 #include "Converter.h"
 
@@ -7,17 +9,27 @@ int main(int argc, const char* argv[])
 {
   try
   {
+    if (argc < 3)
+    {
+      std::cout << "Missing command line arguments. Syntax:\n"
+                << "LI_EU4_Converter.exe [EU4 path] [Output mod name]\n";
+      return 2;
+    }
+    
+    std::string eu4Path = argv[1];
+    std::string eu4ModPath = eu4Path + "\\mod";
+    std::string outputName = argv[2];
+
     std::cout << "Start\n";
 
     Converter converter;
-    converter.CreateMod("TestMod9000",
-                        "G:\\SteamLibrary\\SteamApps\\common\\Europa Universalis IV\\mod",
-                        "G:\\SteamLibrary\\SteamApps\\common\\Europa Universalis IV");
+    converter.CreateMod(outputName, eu4ModPath, eu4Path);
 
     std::cout << "Done\n";
   }
   catch (std::exception& e)
   {
     std::cerr << "Fatal error: " << e.what() << '\n';
+    return 1;
   }
 }
